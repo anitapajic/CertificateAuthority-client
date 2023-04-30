1a84e916af5c08a3ce63bc111d5d2415ffa7ea90
afilipic <75852858+afilipic@users.noreply.github.com>
2023-05-01 01:55:20 +0200

    Login, registration and home page finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
